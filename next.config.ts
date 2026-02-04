0c35f0f99f9525f9a8a649f2046321ac1c917eee
Roseweltty <feijaoguida@hotmail.com>
2026-02-04 09:05:08 -0300

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://162.215.222.208:4000/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
